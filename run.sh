6fea71c701c0de1d109f2f8d4f59c6504b763f48
weichen <weichen@rui-Inspiron-530.(none)>
2014-02-23 01:35:55 -0800

    shell command

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+qemu-system-i386 -M pc -kernel bzImage \
+-drive file=rootfs.ext2,if=ide \
+-append root=/dev/sda -net nic,model=rtl8139 \
+--nographic \
+-net user
+
+#qemu-system-i386 -kernel bzImage \
+#-hda rootfs.ext2 \
+#-boot c \
+#-m 128 \
+#-localtime \
+#-no-reboot \
+#--nographic \
+#-append "root=/dev/sda rw" \
+#-name test_linux \
+#-net nic -net user 
+#-redir tcp:2222::22 \
+#-redir tcp:3333::3333
+
